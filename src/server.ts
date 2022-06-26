9f4cd50dc74a8c77ba0e71fe357eee0d65fdec4a
Arthur Vinicius <thuur.vss@gmail.com>
2022-06-26 01:56:57 -0300

    Implementação de contrato swagger

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,6 @@
 import { app } from ".";
 
-app.listen(3333, () => console.log("Server is running!"));
+app.listen(3333, () => {
+  console.log("Server is running!");
+  console.log("Documentation on http://localhost:3333/api-docs");
+});
